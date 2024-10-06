d6d91fbda89ad75ab7da8fbaafdeb675c6ceebe0
sagar-kumar-yadav <sk5323912@gmail.com>
2024-10-06 19:30:15 +0530

    new commit

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import { Helmet } from "react-helmet";
-import Header from "./Header/Header";
 import Footer from "./Footer";
+import Header from "./Header/Header";
 
 const Layout = ({ children, title, description, keywords, author }) => {
   
